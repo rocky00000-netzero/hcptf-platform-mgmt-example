28d269d149d4d0873d724b7eccb783d19cb2ea80
alexbasista <abasista@hashicorp.com>
2025-04-18 13:40:07 +0900

    add mgmt workspaces

--- main.tf
+++ main.tf
@@ -22,6 +22,17 @@ module "random_pets_1_project" {
   }
 }
 
+module "random_pets_1_mgmt_ws" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.12.0"
+
+  organization   = var.organization
+  workspace_name = "random-pets-1-mgmt-ws"
+  workspace_desc = "Workspace mananging all other Workspaces in this project as code."
+  workspace_tags = ["mgmt"]
+  project_name   = "random-pets-1-application"
+}
+
 module "random_pets_2_project" {
   source  = "alexbasista/projecter/tfe"
   version = "0.1.1"
@@ -33,4 +44,15 @@ module "random_pets_2_project" {
     terraform-platform-team = "admin"
     random-pets-app-team    = "write"
   }
+}
+
+module "random_pets_2_mgmt_ws" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.12.0"
+
+  organization   = var.organization
+  workspace_name = "random-pets-2-mgmt-ws"
+  workspace_desc = "Workspace mananging all other Workspaces in this project as code."
+  workspace_tags = ["mgmt"]
+  project_name   = "random-pets-2-application"
 }
